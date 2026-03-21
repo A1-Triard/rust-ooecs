ae14fcc261635085d90fd16859daf63af5096047
Master <internalmike@gmail.com>
2026-03-22 02:43:06 +0300

    no_std, TestSystem

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
+#![cfg_attr(test, feature(macro_metavar_expr_concat))]
+
+#![no_std]
+
+extern crate alloc;
+
+use alloc::vec::Vec;
 use arena_container::Arena;
+use core::any::TypeId;
+use core::mem::replace;
+use core::ops::{Deref, DerefMut};
 use panicking::panicking;
-use std::any::TypeId;
-use std::mem::replace;
-use std::ops::{Deref, DerefMut};
 
 struct WorldComponent {
     ty: TypeId,
@@ -169,5 +176,97 @@ mod tests {
         assert_eq!(entity.remove_component::<TestComponent>(world, component).value, 8);
         entity.drop_entity(world);
         component.drop_component::<TestComponent>(world);
+    }
+
+    mod test_system {
+        use alloc::rc::Rc;
+        use basic_oop::{import, Vtable, class_unsafe};
+        use super::TestComponent;
+
+        import! { pub test_system:
+            use [obj basic_oop::obj];
+            use crate::*;
+        }
+
+        #[class_unsafe(inherits_Obj)]
+        pub struct TestSystem {
+            list_component: Component,
+            test_component: Component,
+            #[virt]
+            run: fn(head: Entity, world: &mut World),
+        }
+
+        impl TestSystem {
+            pub fn new(
+                list_component: Component,
+                test_component: Component,
+            ) -> Rc<dyn IsTestSystem> {
+                Rc::new(unsafe { Self::new_raw(
+                    list_component,
+                    test_component,
+                    TEST_SYSTEM_VTABLE.as_ptr()
+                ) })
+            }
+
+            pub unsafe fn new_raw(
+                list_component: Component,
+                test_component: Component,
+                vtable: Vtable,
+            ) -> Self {
+                TestSystem {
+                    obj: unsafe { Obj::new_raw(vtable) },
+                    list_component,
+                    test_component,
+                }
+            }
+
+            pub fn run_impl(this: &Rc<dyn IsTestSystem>, head: Entity, world: &mut World) {
+                let list_component = this.test_system().list_component;
+                let test_component = this.test_system().test_component;
+                let mut entity = head;
+                loop {
+                    entity.component::<TestComponent>(world, test_component).unwrap().value += 1;
+                    let Some(next) = entity.component::<List>(world, list_component).unwrap().next else {
+                        break;
+                    };
+                    entity = next;
+                }
+            }
+        }
+
+        pub struct List {
+            pub next: Option<Entity>,
+        }
+    }
+
+    use test_system::*;
+
+    #[test]
+    fn run_system() {
+        let world = &mut World::new();
+        let test_component = Component::new::<TestComponent>(world);
+        let list_component = Component::new::<List>(world);
+
+        let entity_1 = Entity::new(world);
+        let entity_2 = Entity::new(world);
+        let entity_3 = Entity::new(world);
+
+        entity_3.add_component::<List>(world, list_component, List { next: None });
+        entity_2.add_component::<List>(world, list_component, List { next: Some(entity_3) });
+        entity_1.add_component::<List>(world, list_component, List { next: Some(entity_2) });
+
+        entity_1.add_component::<TestComponent>(world, test_component, TestComponent { value: 10 });
+        entity_2.add_component::<TestComponent>(world, test_component, TestComponent { value: 20 });
+        entity_3.add_component::<TestComponent>(world, test_component, TestComponent { value: 30 });
+
+        let system = TestSystem::new(list_component, test_component);
+        system.run(entity_1, world);
+
+        assert_eq!(entity_1.component::<TestComponent>(world, test_component).unwrap().value, 11);
+        assert_eq!(entity_2.component::<TestComponent>(world, test_component).unwrap().value, 21);
+        assert_eq!(entity_3.component::<TestComponent>(world, test_component).unwrap().value, 31);
+
+        test_component.drop_component::<TestComponent>(world);
+        list_component.drop_component::<List>(world);
     }
 }
