8df067c726382b33419ff0df9e594dcd495cf69d
Master <internalmike@gmail.com>
2026-03-22 00:17:01 +0300

    World

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,63 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use arena_container::Arena;
+use std::any::TypeId;
+
+struct WorldComponent {
+    ty: TypeId,
+    storage: Option<(usize, usize, usize, isize)>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct World {
+    components: Vec<WorldComponent>,
+    entities: Arena<usize, Vec<isize>>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub struct Component(usize);
+
+impl World {
+    pub fn new() -> Self {
+        World {
+            components: Vec::new(),
+            entities: Arena::new(),
+        }
     }
 }
+
+impl Component {
+    pub fn new<T: 'static>(world: &mut World) -> Self {
+        let storage: Arena<isize, T> = Arena::new();
+        let storage = storage.into_raw_parts();
+        let ty = TypeId::of::<T>();
+        let id = world.components.len();
+        world.components.push(WorldComponent {
+            ty,
+            storage: Some(storage),
+        });
+        Component(id)
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub struct Entity(usize);
+
+impl Entity {
+    pub fn new(world: &mut World) -> Self {
+        world.entities.insert(|id| (Vec::new(), Entity(id)))
+    }
+
+    pub fn add_component<T: 'static>(self, world: &mut World, component: Component, t: T) {
+        assert_eq!(world.components[component.0].ty, TypeId::of::<T>());
+        let storage = world.components[component.0].storage.take().unwrap();
+        let mut storage: Arena<isize, T> = unsafe {
+            Arena::from_raw_parts(storage.0, storage.1, storage.2, storage.3)
+        };
+        let id = storage.insert(move |id| (t, id));
+        world.components[component.0].storage = Some(storage.into_raw_parts());
+        let components = &mut world.entities[self.0];
+        for _ in components.len() ..= component.0 {
+            components.push(-1);
+        }
+        components[component.0] = id;
+    }
+}
+
