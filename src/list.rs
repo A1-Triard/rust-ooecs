05bcdbb7e4da7c9928835a76baa60ac9e2a921d0
Master <internalmike@gmail.com>
2026-03-22 11:38:15 +0300

    list: unify api with tree

--- src/list.rs
+++ src/list.rs
@@ -1,3 +1,4 @@
+use core::mem::replace;
 use crate::{Entity, Component, World};
 
 pub struct List {
@@ -5,39 +6,39 @@ pub struct List {
 }
 
 impl List {
-    pub fn insert(head: &mut Option<Entity>, node: Entity, world: &mut World, list_component: Component) {
-        if let &mut Some(head_entity) = head {
-            let mut prev = head_entity;
-            loop {
-                let next = prev.component::<List>(world, list_component).unwrap().next;
-                if next == head_entity { break; }
-                prev = next;
-            }
-            prev.component::<List>(world, list_component).unwrap().next = node;
-            node.add_component::<List>(world, list_component, List { next: head_entity });
-            *head = Some(node);
-        } else {
-            node.add_component::<List>(world, list_component, List { next: node });
-            *head = Some(node);
+    pub fn init(node: Entity, world: &mut World, list_component: Component) {
+        node.add_component::<List>(world, list_component, List { next: node });
+    }
+
+    pub fn destroy(node: Entity, world: &mut World, list_component: Component) {
+        let mut n = node;
+        loop {
+            let list = n.remove_component::<List>(world, list_component);
+            n = list.next;
+            if n == node { break; }
         }
     }
 
-    pub fn remove(head: &mut Option<Entity>, node: Entity, world: &mut World, list_component: Component) {
-        let mut prev = node;
+    pub fn add(prev: Entity, node: Entity, world: &mut World, list_component: Component) {
+        let mut last = node;
+        loop {
+            let next = last.component::<List>(world, list_component).unwrap().next;
+            if next == node { break; }
+            last = next;
+        }
+        let next = replace(&mut prev.component::<List>(world, list_component).unwrap().next, node);
+        last.component::<List>(world, list_component).unwrap().next = next;
+    }
+
+    pub fn remove(from: Entity, to: Entity, world: &mut World, list_component: Component) {
+        let mut prev = to;
         loop {
             let next = prev.component::<List>(world, list_component).unwrap().next;
-            if next == node { break; }
+            if next == from { break; }
             prev = next;
         }
-        if prev == node {
-            node.remove_component::<List>(world, list_component);
-            *head = None;
-        } else {
-            let next = node.remove_component::<List>(world, list_component).next;
-            prev.component::<List>(world, list_component).unwrap().next = next;
-            if *head == Some(node) {
-                *head = Some(next);
-            }
-        }
+        assert_ne!(prev, to);
+        let next = replace(&mut to.component::<List>(world, list_component).unwrap().next, from);
+        prev.component::<List>(world, list_component).unwrap().next = next;
     }
 }
