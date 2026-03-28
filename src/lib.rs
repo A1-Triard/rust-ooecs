2f56bfe57b4e2b08f826a18f8bf49c291271389b
Master <internalmike@gmail.com>
2026-03-29 01:18:25 +0300

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,22 @@
 #![doc(test(attr(allow(unused_variables))))]
 #![allow(clippy::collapsible_if)]
 
+//! This implementation of the ECS differs from the traditional approach in several respects
+//! and is intended for use in specific scenarios where its strengths are important
+//! and its weaknesses are irrelevant.
+//!
+//! The first difference is that archetypes form an inheritance tree.
+//! So, if you have three components, you cannot simultaneously have an entity
+//! containing only components 1 and 2, an entity containing components 1 and 3,
+//! and an entity containing only components 2 and 3.
+//!
+//! The second difference is that there are no systems in the traditional sense,
+//! where they are automatically applied to all entities containing the required components.
+//! Instead, you must explicitly organize the lists of entities
+//! (or other structures, such as trees) that the systems will work with.
+//!
+//! See the README for a usage example.
+
 #![no_std]
 
 extern crate alloc;
@@ -118,9 +134,9 @@ impl<E: PointeeSized> Drop for World<E> {
 /// and all base components along the chain.
 #[derive(Educe)]
 #[educe(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
-pub struct Component<E: PointeeSized + 'static>(usize, PhantomType<&'static E>);
+pub struct Component<T, E: PointeeSized + 'static>(usize, PhantomType<(T, &'static E)>);
 
-impl<E: PointeeSized> Component<E> {
+impl<T: 'static, E: PointeeSized> Component<T, E> {
     /// Register new [`Component`] and corresponding archetype.
     ///
     /// An archetype is a collection of [`Component`]s. The registered archetype contains
@@ -144,20 +160,59 @@ impl<E: PointeeSized> Component<E> {
     ///
     /// # fn main() {
     /// let mut world = <World<Game>>::new();
-    /// let position = Component::new::<Position>(None, &mut world);
-    /// let velocity = Component::new::<Velocity>(Some(position), &mut world);
+    /// let position: Component<Position, Game> = Component::new_base(&mut world);
+    /// let velocity: Component<Velocity, Game> = Component::new(position, &mut world);
     /// # }
     /// ```
     ///
     /// Here we define two `Component`s: `position` and `velocity`, and two corresponding archetypes.
     /// The `position` archetype contains `position` component only. The `velocity` archetype
     /// contains two components: `velocity` and `position`.
-    pub fn new<T: 'static>(base: Option<Component<E>>, world: &mut World<E>) -> Self {
+    pub fn new<B>(base: Component<B, E>, world: &mut World<E>) -> Self {
+        Self::new_raw(Some(base.0), world)
+    }
+
+    /// Register new [`Component`] and corresponding base archetype.
+    ///
+    /// An archetype is a collection of [`Component`]s. The registered base archetype contains
+    /// of corresponding component only.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # use ooecs::{World, Component}; 
+    /// pub struct Position {
+    ///     x: i16,
+    ///     y: i16,
+    /// }
+    ///
+    /// pub struct Velocity {
+    ///     x: i16,
+    ///     y: i16,
+    /// }
+    ///
+    /// pub enum Game { }
+    ///
+    /// # fn main() {
+    /// let mut world = <World<Game>>::new();
+    /// let position: Component<Position, Game> = Component::new_base(&mut world);
+    /// let velocity: Component<Velocity, Game> = Component::new(position, &mut world);
+    /// # }
+    /// ```
+    ///
+    /// Here we define two `Component`s: `position` and `velocity`, and two corresponding archetypes.
+    /// The `position` archetype contains `position` component only. The `velocity` archetype
+    /// contains two components: `velocity` and `position`.
+    pub fn new_base(world: &mut World<E>) -> Self {
+        Self::new_raw(None, world)
+    }
+
+    fn new_raw(base: Option<usize>, world: &mut World<E>) -> Self {
         let drop_component = Box::new(|p: *mut u8| {
             drop(unsafe { ptr::read(p as *mut T) });
         });
         let info = if let Some(base) = base {
-            let base_info = &world.components[base.0];
+            let base_info = &world.components[base];
             let archetype_align = max(
                 max(base_info.archetype_align, align_of::<T>()),
                 align_of::<Option<usize>>()
@@ -173,7 +228,7 @@ impl<E: PointeeSized> Component<E> {
             let index = base_info.index.checked_add(1).unwrap();
             let mut archetype_components_except_self = base_info.archetype_components_except_self.clone();
             archetype_components_except_self.reserve_exact(1);
-            archetype_components_except_self.push(base.0);
+            archetype_components_except_self.push(base);
             ComponentInfo {
                 ty: TypeId::of::<T>(),
                 drop_component,
@@ -229,7 +284,7 @@ impl<E: PointeeSized> Entity<E> {
     /// the [`add`](Entity::add) method.
     ///
     /// An [`Entity`] cannot contains [`Component`] that does not belong to the `archetype`.
-    pub fn new(archetype: Component<E>, world: &mut World<E>) -> Self {
+    pub fn new<T>(archetype: Component<T, E>, world: &mut World<E>) -> Self {
         let info = &mut world.components[archetype.0];
         let index = if let Some(vacancy) = info.archetype_storage_vacancy {
             let cell = unsafe { info.archetype_storage_ptr.add(info.archetype_size * vacancy) };
@@ -312,7 +367,7 @@ impl<E: PointeeSized> Entity<E> {
     ///
     /// Each component can only be added once. All components belonging to the entity archetype
     /// must be added before the first [`get`](Entity::get)/[`get_mut`](Entity::get_mut) call.
-    pub fn add<T: 'static>(self, component: Component<E>, world: &mut World<E>, data: T) {
+    pub fn add<T: 'static>(self, component: Component<T, E>, world: &mut World<E>, data: T) {
         let e_info = &mut world.entities[self.0];
         let c_info = &world.components[component.0];
         assert_eq!(c_info.ty, TypeId::of::<T>(), "component type mismatch");
@@ -333,7 +388,12 @@ impl<E: PointeeSized> Entity<E> {
         unsafe { ptr::write(p, data); }
     }
 
-    pub fn get<T: 'static>(self, component: Component<E>, world: &World<E>) -> Option<&T> {
+    /// Access entity data.
+    ///
+    /// Return reference to the entity component data.
+    /// Returns [`None`] if provided component does not belong to the entity archetype.
+    /// Panics if the entity is not fully initialized (i. e. does not contain all required components).
+    pub fn get<T: 'static>(self, component: Component<T, E>, world: &World<E>) -> Option<&T> {
         let e_info = &world.entities[self.0];
         let c_info = &world.components[component.0];
         assert_eq!(c_info.ty, TypeId::of::<T>(), "component type mismatch");
@@ -351,7 +411,12 @@ impl<E: PointeeSized> Entity<E> {
         Some(unsafe { &*(p as *mut T) })
     }
 
-    pub fn get_mut<T: 'static>(self, component: Component<E>, world: &mut World<E>) -> Option<&mut T> {
+    /// Access entity data.
+    ///
+    /// Return mutable reference to the entity component data.
+    /// Returns [`None`] if provided component does not belong to the entity archetype.
+    /// Panics if the entity is not fully initialized (i. e. does not contain all required components).
+    pub fn get_mut<T: 'static>(self, component: Component<T, E>, world: &mut World<E>) -> Option<&mut T> {
         let e_info = &world.entities[self.0];
         let c_info = &world.components[component.0];
         assert_eq!(c_info.ty, TypeId::of::<T>(), "component type mismatch");
@@ -386,24 +451,24 @@ mod tests {
     #[test]
     fn create_world_reg_component_drop_world() {
         let mut world = <World<X>>::new();
-        let _position = Component::new::<Position>(None, &mut world);
+        let _position: Component<Position, X> = Component::new_base(&mut world);
         drop(world);
     }
 
     #[test]
     fn create_entity_modify_check() {
         let world = &mut <World<X>>::new();
-        let position = Component::new::<Position>(None, world);
-        let velocity = Component::new::<Velocity>(Some(position), world);
+        let position: Component<Position, X> = Component::new_base(world);
+        let velocity: Component<Velocity, X> = Component::new(position, world);
         let entity = Entity::new(velocity, world);
         entity.add(position, world, Position { x: 0 });
         entity.add(velocity, world, Velocity { x: 1 });
-        assert_eq!(entity.get::<Position>(position, world).unwrap().x, 0);
-        entity.get_mut::<Position>(position, world).unwrap().x = 10;
-        assert_eq!(entity.get::<Position>(position, world).unwrap().x, 10);
-        assert_eq!(entity.get::<Velocity>(velocity, world).unwrap().x, 1);
-        entity.get_mut::<Velocity>(velocity, world).unwrap().x = -1;
-        assert_eq!(entity.get::<Velocity>(velocity, world).unwrap().x, -1);
+        assert_eq!(entity.get(position, world).unwrap().x, 0);
+        entity.get_mut(position, world).unwrap().x = 10;
+        assert_eq!(entity.get(position, world).unwrap().x, 10);
+        assert_eq!(entity.get(velocity, world).unwrap().x, 1);
+        entity.get_mut(velocity, world).unwrap().x = -1;
+        assert_eq!(entity.get(velocity, world).unwrap().x, -1);
     }
 
     static COMPONENT_IMPL_DROP_ALIVE: AtomicIsize = AtomicIsize::new(0);
@@ -426,7 +491,7 @@ mod tests {
     #[test]
     fn drop_components() {
         let mut world = <World<X>>::new();
-        let component = Component::new::<ComponentImplDrop>(None, &mut world);
+        let component: Component<ComponentImplDrop, X> = Component::new_base(&mut world);
         let entity_1 = Entity::new(component, &mut world);
         entity_1.add(component, &mut world, ComponentImplDrop::new());
         let entity_2 = Entity::new(component, &mut world);
